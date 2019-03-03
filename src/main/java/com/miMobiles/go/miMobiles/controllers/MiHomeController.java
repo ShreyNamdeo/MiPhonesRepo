8cd09b7ec94bbc8330db988ab25292ef5ef4eda5
ShreyNamdeo <shreynamdeo@gmail.com>
2019-03-04 00:13:53 +0530

    adding ui files

--- src/main/java/com/miMobiles/go/miMobiles/controllers/MiHomeController.java
+++ src/main/java/com/miMobiles/go/miMobiles/controllers/MiHomeController.java
@@ -29,4 +29,28 @@ public class MiHomeController {
         model.addAttribute("title","Gadget-Meter Studios");
         return "login";
     }
+
+    @RequestMapping(value = "/adminProductList")
+    public String adminProductList(Model model){
+        model.addAttribute("title","Gadget-Meter Studios");
+        return "AdminProductList";
+    }
+
+    @RequestMapping(value = "/productDetails")
+    public String productDetails(Model model){
+        model.addAttribute("title","Gadget-Meter Studios");
+        return "productDetails";
+    }
+
+    @RequestMapping(value = "/productDetailsCompare")
+    public String productDetailsCompare(Model model){
+        model.addAttribute("title","Gadget-Meter Studios");
+        return "productDetailsCompare";
+    }
+
+    @RequestMapping(value = "/addProduct")
+    public String addProduct(Model model){
+        model.addAttribute("title","Gadget-Meter Studios");
+        return "addProduct";
+    }
 }
